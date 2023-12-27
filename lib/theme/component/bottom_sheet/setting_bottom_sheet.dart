afdaa9f595b8f4990e38401c3f3c3172d5325837
고명민 <2022795@vieworks.com>
2023-12-28 08:41:46 +0900

    toggleLang 기능이 있는 language 타이틀을 BottomSheet에 추가

--- lib/theme/component/bottom_sheet/setting_bottom_sheet.dart
+++ lib/theme/component/bottom_sheet/setting_bottom_sheet.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/src/service/lang_service.dart';
 import 'package:house_of_tomorrow/src/service/theme_service.dart';
 import 'package:house_of_tomorrow/theme/component/bottom_sheet/base_bottom_sheet.dart';
 import 'package:house_of_tomorrow/theme/component/tile.dart';
+import 'package:house_of_tomorrow/util/helper/intl_helper.dart';
 import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
 import 'package:provider/provider.dart';
 
@@ -11,6 +13,7 @@ class SettingBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bool isLightTheme = context.theme.brightness == Brightness.light;
+    final LangService langService = context.watch();
     return BaseBottomSheet(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -21,6 +24,14 @@ class SettingBottomSheet extends StatelessWidget {
             title: S.current.theme,
             subtitle: isLightTheme ? S.current.light : S.current.dark,
             onPressed: context.read<ThemeService>().toggleTheme,
+          ),
+
+          /// Lang Tile
+          Tile(
+            icon: 'language',
+            title: S.current.language,
+            subtitle: IntlHelper.isKo ? S.current.ko : S.current.en,
+            onPressed: langService.toggleLang,
           ),
         ],
       ),

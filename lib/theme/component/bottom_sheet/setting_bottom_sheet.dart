59f093a82f57f91c2173dd0446bf0e3db4781a0c
고명민 <2022795@vieworks.com>
2023-12-27 17:20:48 +0900

    커스텀 Tile클래스를 생성하고 SettingBottomSheet에서 Tile 위젯을 이용하여 테마 설정을 변경할 수 있도록 구현

--- lib/theme/component/bottom_sheet/setting_bottom_sheet.dart
+++ lib/theme/component/bottom_sheet/setting_bottom_sheet.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:house_of_tomorrow/src/service/theme_service.dart';
 import 'package:house_of_tomorrow/theme/component/bottom_sheet/base_bottom_sheet.dart';
+import 'package:house_of_tomorrow/theme/component/tile.dart';
+import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
+import 'package:provider/provider.dart';
 
 class SettingBottomSheet extends StatelessWidget {
   const SettingBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const BaseBottomSheet(
-      child: Text("Hello bottom sheet"),
+    final bool isLightTheme = context.theme.brightness == Brightness.light;
+    return BaseBottomSheet(
+      child: Column(
+        children: [
+          /// Theme Tile
+          Tile(
+            icon: isLightTheme ? 'sunny' : 'moon',
+            title: S.current.theme,
+            subtitle: isLightTheme ? S.current.light : S.current.dark,
+            onPressed: context.read<ThemeService>().toggleTheme,
+          ),
+        ],
+      ),
     );
   }
 }
